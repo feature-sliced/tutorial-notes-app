d1b47a457b3451366c039a0d24d08a156a6d36ba
Lev Chelyadinov <leva181777@gmail.com>
2023-09-17 20:51:24 +0200

    Add a link version of a button

--- app/shared/ui/Button.tsx
+++ app/shared/ui/Button.tsx
@@ -1,5 +1,6 @@
 import type { ComponentProps } from "react";
 import { cva } from "class-variance-authority";
+import { Link, type LinkProps } from "@remix-run/react";
 
 const buttonStyles = cva(
   "p-2 inline-flex gap-2 items-center font-semibold rounded-lg bg-white dark:bg-transparent",
@@ -26,4 +27,10 @@ export function Button({
       {...props}
     />
   );
+}
+
+export function ButtonLink(props: LinkProps) {
+  // This rule is for the consumer to follow
+  // eslint-disable-next-line jsx-a11y/anchor-has-content
+  return <Link className={buttonStyles()} {...props} />;
 }
--- app/shared/ui/index.ts
+++ app/shared/ui/index.ts
@@ -1,3 +1,3 @@
-export { Button } from "./Button";
+export { Button, ButtonLink } from "./Button";
 export { TextField } from "./TextField";
 export { Card } from "./Card";
