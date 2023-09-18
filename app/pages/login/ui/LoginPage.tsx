ed00715d58dc2141e9e1d694a855499227300afb
Lev Chelyadinov <leva181777@gmail.com>
2023-09-18 23:00:00 +0200

    Implement the login page

--- app/pages/login/ui/LoginPage.tsx
+++ app/pages/login/ui/LoginPage.tsx
@@ -1,3 +1,34 @@
 export function LoginPage() {
-  return <>Login page</>;
+  return (
+    <div className="px-6 py-10 text-xl text-neutral-900 dark:text-neutral-300">
+      <div className="px-2">
+        <h1 className="text-3xl font-semibold mb-2">Notes</h1>
+        <p className="mb-4">
+          A web app built with Remix and architected with Feature-Sliced Design
+        </p>
+        <ul className="pl-8 list-disc">
+          <li>
+            <a
+              className="text-blue-600 dark:text-blue-300 underline"
+              href="https://github.com/feature-sliced/tutorial-notes-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              View source
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-blue-600 dark:text-blue-300 underline"
+              href="https://feature-sliced.design"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Read the FSD docs
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
